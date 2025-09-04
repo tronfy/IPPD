ab398cbae2f2b233957b255ed5d3c4591bb0542c
Nic Schmidt <nicolasdschmidt@proton.me>
2025-09-04 19:58:31 +0000

    aula4: traprule_par

---
+++ 4/traprule_par.c
@@ -0,0 +1,36 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+double f(double x) { return pow(x, 10) + pow(x, 3) + 63; }
+
+int main() {
+  double a = 1, b = 500;
+  int n = 100000000;
+  double h = (b - a) / n;
+  struct timeval start, stop;
+  int i;
+
+  gettimeofday(&start, NULL);
+
+  double approx = (f(a) + f(b)) / 2.0;
+
+#pragma omp parallel for reduction(+ : approx)
+  for (i = 1; i <= n - 1; i++) {
+    approx += f(a + i * h);
+  }
+
+  approx = h * approx;
+
+  gettimeofday(&stop, NULL);
+
+  double tempo =
+      (((double)(stop.tv_sec) * 1000.0 + (double)(stop.tv_usec / 1000.0)) -
+       ((double)(start.tv_sec) * 1000.0 + (double)(start.tv_usec / 1000.0)));
+
+  fprintf(stdout, "Area do trapezio = %g\n", approx);
+  fprintf(stdout, "Tempo total gasto = %g ms\n", tempo);
+
+  return 0;
+}
